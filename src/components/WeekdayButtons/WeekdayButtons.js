257d945effc2f1eff1331afb251b375ef62e926b
tayloroxelgren <68982134+tayloroxelgren@users.noreply.github.com>
2023-02-22 21:58:03 -0500

    Update WeekdayButtons.js
    Added function I was using while testing.

--- src/components/WeekdayButtons/WeekdayButtons.js
+++ src/components/WeekdayButtons/WeekdayButtons.js
@@ -2,13 +2,26 @@ import "./WeekdayButtons.css";
 // Coded by Taylor Oxelgren
 
 /* This function return buttons for all days of the week and should be modular to use on other profile types*/
-export default function WeekdayButtons(){
-    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+/* can be used for other buttons as well*/
+export default function WeekdayButtons(props){
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let it=props.prop;
+    if(Array.isArray(it)){
+        return (
+            <div className="buttonFrame">
+                {/* Maps each day to the function call, which produces a button */}
+                {it.map(function(itr,keyid) {
+                    return <button key={keyid} className="weekdayButtons">{itr}</button>;
+                })}
+            </div>
+          );
+    }
+
     return (
         <div className="buttonFrame">
             {/* Maps each day to the function call, which produces a button */}
-            {days.map(function(day) {
-                return <button className="weekdayButtons">{day}</button>;
+            {days.map(function(day,keyid) {
+                return <button key={keyid} className="weekdayButtons">{day}</button>;
             })}
         </div>
       );
