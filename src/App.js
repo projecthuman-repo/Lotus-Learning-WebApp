24734a3fef034981db7cdcb33ea1184e52f96835
Humza-A <humzaaamir61@gmail.com>
2023-03-11 21:20:12 -0500

    Added: Profile-Learners-Experienced Account - User Info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-import CourseBox from "./components/Course-Box/CourseBox";
+//import CourseBox from "./components/Course-Box/CourseBox";
 import Header from "./components/Header-Components/Logged-In/Header-Logged-In";
 // import SideNav from "./components/Side-Nav-bar/Side-Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PersonalProfile from "./components/Personal-Profile-Components/PersonalProfile";
+import LearnersInfo from "./components/Profile-Learners-Info/LearnersInfo";
+// import PersonalProfile from "./components/Personal-Profile-Components/PersonalProfile";
 
 
 function App() {
   return (
     <>
       <Header />
-      <CourseBox />
+      <LearnersInfo />
     </>
   );
 }
