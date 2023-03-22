6253929614370cb737c9bd796279b09a80a28322
tayloroxelgren <t.oxelgren@gmail.com>
2023-03-22 16:13:44 -0400

    Changed router to match main

--- src/router.js
+++ src/router.js
@@ -2,10 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import LearnersProfile from "./Pages/Learners-Profile/Learners-Profile";
 import SignUpPage from "./Pages/Sign-Up-Page/SignUpPage";
-import PersonalProfile from "./components/Personal-Profile-Components/PersonalProfile";
-import SearchBar from "./components/SearchBar-Components/SearchBar";
+import CourseProgressPage2 from "./Pages/Learners-CourseProgress/CourseProgressPage";
+// import SearchBar from "./components/SearchBar-Components/SearchBar";
+// import CourseProgessPage from "./Pages/Learners-CourseProgress/CourseProgressPage.js";
+import LearnersHelp from "./Pages/Learners-Help/LearnersHelp";
 
-import ProfileMyCourses from "./Pages/Learners-MyCourses/ProfileMyCourses";
+import LearnersProfileNotifications from "./Pages/Learners-Profile-Notifications/LearnersProfileNotifications";
+import PrivacySecurity from "./Pages/Learners-Privacy_Security/PrivacySecurity";
+import LearnersAccountPage from "./Pages/Learners-Account/LearnersAccount";
+import CourseSpecific from "./Pages/Course-Specific/CourseSpecific";
+import LearnersSignUp from "./Pages/Learners-Sign-Up/Learners-Sign-Up";
+import LearnersGamesPage from "./Pages/Learners-Games/LearnersGamesPage";
 
 export default function Router() {
   return (
@@ -13,11 +20,20 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/LearnersSignUp" element={<LearnersSignUp />} />
+        <Route path="/personalprofile" element={<LearnersAccountPage />} />
+        {/* <Route path="/CourseProgress" element={<CourseProgessPage />} /> */}
+        <Route path="/LearnersHelp" element={<LearnersHelp />} />
         <Route path="/coursecatalogue" element={<LearnersProfile />} />
-        <Route path="/personalprofile" element={<PersonalProfile />} />
-        <Route path="/searchbar" element={<SearchBar />} />
+        <Route path="/CourseProgressPage2" element={<CourseProgressPage2 />} />
+        <Route
+          path="/LearnersNotifications"
+          element={<LearnersProfileNotifications />}
+        />
+        <Route path="/Security" element={<PrivacySecurity />} />
+        <Route path="/CourseSpecific" element={<CourseSpecific />} />
+        <Route path="/LearnersGamesPage" element={<LearnersGamesPage />} />
 
-        <Route path="/bookmarkedcourses" element={<ProfileMyCourses />} />
       </Routes>
     </BrowserRouter>
   );
