e1d2bb9c9f5daa568ab5770032a9ae4583804849
Humza-A <humzaaamir61@gmail.com>
2023-03-22 14:44:09 -0400

    Added: more side-nav Links

--- src/router.js
+++ src/router.js
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import LearnersProfile from "./Pages/Learners-Profile/Learners-Profile";
 import SignUpPage from "./Pages/Sign-Up-Page/SignUpPage";
-import PersonalProfile from "./components/Personal-Profile-Components/PersonalProfile";
-import SearchBar from "./components/SearchBar-Components/SearchBar";
-import CourseProgessPage from "./Pages/Learners-CourseProgress/CourseProgressPage.js";
+import CourseProgressPage2 from "./Pages/Learners-CourseProgress/CourseProgressPage";
+// import SearchBar from "./components/SearchBar-Components/SearchBar";
+// import CourseProgessPage from "./Pages/Learners-CourseProgress/CourseProgressPage.js";
+import LearnersHelp from "./Pages/Learners-Help/LearnersHelp";
 
 import LearnersProfileNotifications from "./Pages/Learners-Profile-Notifications/LearnersProfileNotifications";
 import PrivacySecurity from "./Pages/Learners-Privacy_Security/PrivacySecurity";
@@ -20,13 +21,14 @@ export default function Router() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/LearnersSignUp" element={<LearnersSignUp />} />
         <Route path="/personalprofile" element={<LearnersAccountPage />} />
+        {/* <Route path="/CourseProgress" element={<CourseProgessPage />} /> */}
+        <Route path="/LearnersHelp" element={<LearnersHelp />} />
         <Route path="/coursecatalogue" element={<LearnersProfile />} />
-        <Route path="/searchbar" element={<SearchBar />} />
+        <Route path="/CourseProgressPage2" element={<CourseProgressPage2 />} />
         <Route
           path="/LearnersNotifications"
           element={<LearnersProfileNotifications />}
         />
-        <Route path="/CourseProgress" element={<CourseProgessPage />} />
         <Route path="/Security" element={<PrivacySecurity />} />
         <Route path="/CourseSpecific" element={<CourseSpecific />} />
 
