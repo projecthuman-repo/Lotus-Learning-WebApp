32553ed94866c80f5dc3616efdc88a8f651dfd3d
Juan Buitrago <buitr4go@gmail.com>
2023-11-14 12:44:50 -0800

    updated profile home design

--- frontend/src/Pages/Learners/ProfileScreens/Profile-Home/ProfileHome.js
+++ frontend/src/Pages/Learners/ProfileScreens/Profile-Home/ProfileHome.js
@@ -8,15 +8,15 @@ import AddIcon from "@mui/icons-material/Add";
 import { Link } from "react-router-dom";
 
 const ProfileHome = ({ courses }) => {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  // const daysOfWeek = [
+  //   "Sunday",
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  // ];
 
   const badges = ["Coding HTML", "Algebra III", "Algebra I"];
 
