ffa8024e9bdf1dfe3f85a3cae2fc88d22029a660
Yuta Kusuno <yutakusuno51@gmail.com>
2024-01-11 15:08:13 -0800

    Add new endpoints to update and delete users

--- backend/graphql/resolvers/auth.js
+++ backend/graphql/resolvers/auth.js
@@ -1,7 +1,50 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
+
+// Utility function to check if the user is authenticated and authorized
+const checkAuth = (req) => {
+  if (!req.isAuth) {
+    throw new Error("Unauthenticated!");
+  }
+};
+
+// Resolver function to update user details
+const updateUser = async (args, req) => {
+  checkAuth(req); // Ensure the user is authenticated
+
+  const userId = args.id; // Get the user ID from the arguments
+  const userUpdates = args.userInput;
+
+  // Hash the new password if it's being changed
+  if (userUpdates.password) {
+    userUpdates.password = await bcrypt.hash(userUpdates.password, 12);
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(userId, userUpdates, {
+    new: true,
+  });
+  if (!updatedUser) {
+    throw new Error("User not found.");
+  }
+
+  return { ...updatedUser._doc, password: null }; // Do not return the password
+};
+
+// Resolver function to delete a user
+const deleteUser = async (args, req) => {
+  checkAuth(req); // Ensure the user is authenticated
+
+  const userId = args.id; // Get the user ID from the arguments
+
+  const deletedUser = await User.findByIdAndDelete(userId);
+  if (!deletedUser) {
+    throw new Error("User not found.");
+  }
+
+  return { ...deletedUser._doc, password: null }; // Do not return the password
+};
 
 module.exports = {
   /* The `createUser` function is an asynchronous function that takes an argument `args`. It is
@@ -11,7 +54,7 @@ module.exports = {
       const existingUser = await User.findOne({ email: args.userInput.email });
 
       if (existingUser) {
-        throw new Error('User exists already.');
+        throw new Error("User exists already.");
       }
 
       const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
@@ -40,12 +83,12 @@ module.exports = {
     try {
       const user = await User.findOne({ email: email });
       if (!user) {
-        throw new Error('User does not exist!');
+        throw new Error("User does not exist!");
       }
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        throw new Error('Password is incorrect!');
+        throw new Error("Password is incorrect!");
       }
 
       const token = jwt.sign(
@@ -53,9 +96,9 @@ module.exports = {
           userId: user.id,
           email: user.email,
         },
-        'secretkey',
+        "secretkey",
         {
-          expiresIn: '1h',
+          expiresIn: "1h",
         }
       );
       return { userId: user.id, token: token, tokenExpiration: 1 };
@@ -63,4 +106,7 @@ module.exports = {
       throw err;
     }
   },
+  // Adding the new resolvers here
+  updateUser,
+  deleteUser,
 };
