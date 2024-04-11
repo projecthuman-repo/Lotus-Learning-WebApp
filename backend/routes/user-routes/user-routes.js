964e15257b09f268d8ea9e77c3fe09aec765af14
Gaurav3023 <44793171+Gaurav3023@users.noreply.github.com>
2024-04-11 15:58:55 -0400

    user email and username validation completed

--- backend/routes/user-routes/user-routes.js
+++ backend/routes/user-routes/user-routes.js
@@ -1,68 +1,61 @@
 const express = require('express');
 const User = require("../../models/User.js");
-const {logInUser} = require('../../controllers/user/user-login-logout.js')
+const { logInUser } = require('../../controllers/user/user-login-logout.js');
 const router = express.Router();
-
-router.post(
-    "/create-user",
-     async (req, res, next) => {
-      try {
-        const newUser = req.body
-        
-        const existingUser = await User.findOne({ email: newUser.email });
-        if (existingUser) {
-          return res.status(200).json({
-            success: false,
-            message: 'The email is already in use',
-          });
+router.post("/create-user", async(req, res, next) => {
+    try {
+        const newUser = req.body;
+        // Check if the email is already in use
+        const existingUserByEmail = await User.findOne({ email: newUser.email });
+        if (existingUserByEmail) {
+            return res.status(200).json({
+                success: false,
+                message: 'The email is already in use',
+            });
         }
-
+        // Check if the username is already taken
+        const existingUserByUsername = await User.findOne({ username: newUser.username });
+        if (existingUserByUsername) {
+            return res.status(200).json({
+                success: false,
+                message: 'The username is already taken',
+            });
+        }
+        // If both checks pass, proceed to create the user
         const user = new User({
-          firstName: newUser.firstName,
-          lastName: newUser.lastName,
-          accountType: newUser.accountType,
-          username: newUser.username,
-          email: newUser.email,
-          name: newUser.username,
-          password: newUser.password,
-
-
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            accountType: newUser.accountType,
+            username: newUser.username,
+            email: newUser.email,
+            password: newUser.password,
         });
-        await user.save().then(() => {
-          return res.status(200).json({
+        await user.save();
+        return res.status(200).json({
             success: true,
             user: user,
-          });
-        })
-  
-
-      } catch (error) {
-        return next(error.message);
-      }
+        });
+    } catch (error) {
+        return next(error);
     }
-  );
-
+});
 router.post('/login-user', async(req, res, next) => {
-  try{
-    const loginUser = req.body
-    const foundUser = await logInUser(loginUser.email, loginUser.password);
-
-    if(foundUser.success){
-      return res.status(200).json({
-        success: true,
-        user: foundUser.user
-      });
-    }else{
-      return res.status(400).json({
-        success: false,
-        error: foundUser.msg
-      });
+    try {
+        const loginUser = req.body;
+        const foundUser = await logInUser(loginUser.email, loginUser.password);
+        if (foundUser.success) {
+            return res.status(200).json({
+                success: true,
+                user: foundUser.user
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                error: foundUser.msg
+            });
+        }
+    } catch (error) {
+        return next(error);
     }
-
-  }catch (error) {
-    return next(error.message);
-  }
-})
-
+});
 module.exports = router;
-
