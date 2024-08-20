513cc02f4acba65ff3a77ffcd826f529470fa7c2
divyansh-PHC <divyanshpanwar.dev@projecthumancity.com>
2024-08-20 23:35:55 +0530

    made the Learning course page responsive

--- frontend/src/Pages/newPages/coursePage/LearningPageCourse/LearningCoursePage.jsx
+++ frontend/src/Pages/newPages/coursePage/LearningPageCourse/LearningCoursePage.jsx
@@ -23,24 +23,34 @@ const LearningCoursePage = () => {
     try {
       const res = await getCourseData(id);
       setCourseData(res.data);
-      setSelectedLesson(res.data.lessons[0])
+      setSelectedLesson(res.data.lessons[0]);
       console.log(res.data);
       setLoaded(true);
     } catch (error) {
       console.error(error.message);
     }
   };
+
   return (
     <div className="w-full h-full">
       <>
         {loaded ? (
-          <div className="h-full flex w-full justify-between">
-            <LearningPageSideMenu  courseLessons={courseData.lessons} selectedLesson={selectedLesson} setSelectedLesson={setSelectedLesson}/>
-            <LearningMainContent selectedLesson={selectedLesson} gameData={courseData.lessons[0].lessonContent.linked_game.gameRes}/>
+          <div className="h-full flex flex-col md:flex-row w-full justify-between">
+            <LearningPageSideMenu
+              courseLessons={courseData.lessons}
+              selectedLesson={selectedLesson}
+              setSelectedLesson={setSelectedLesson}
+              className="w-full md:w-1/4"
+            />
+            <LearningMainContent
+              selectedLesson={selectedLesson}
+              gameData={courseData.lessons[0].lessonContent.linked_game.gameRes}
+              className="w-full md:w-3/4"
+            />
           </div>
         ) : (
           <div className="h-full w-full flex items-center justify-center">
-            <BarLoader/>
+            <BarLoader />
           </div>
         )}
       </>
